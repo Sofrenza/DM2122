e4cc9031ea6694fe571468b402972ab16048b7f9
Jiawei <200534U@mymail.nyp.edu.sg>
2020-11-04 22:22:41 +0800

    Retrying this AGAIN, stuff from Attempt 3 was cloned, replacing stuff from original attempt to make attempt 4

--- Application/Source/Scene1.cpp
+++ Application/Source/Scene1.cpp
@@ -14,67 +14,141 @@ Scene1::~Scene1()
 
 void Scene1::Init()
 {
-	m_programID = LoadShaders("Shader//SimpleVertexShader.vertexshader", "Shader//SimpleFragmentShader.fragmentshader");
+
+	//Load vertex and fragment shaders
+	m_programID = LoadShaders(
+		"Shader//SimpleVertexShader.vertexshader",
+		"Shader//SimpleFragmentShader.fragmentshader");
 	glUseProgram(m_programID);
 
-	//Switch background colour to dark blue
+	//set background colour to dark blue
 	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
 
-	//generating a default VAO
+	//Generate a default VAO for now
 	glGenVertexArrays(1, &m_vertexArrayID);
 	glBindVertexArray(m_vertexArrayID);
 
-	//generate buffers
+	//Generate Buffers
 	glGenBuffers(NUM_GEOMETRY, &m_vertexBuffer[0]);
 	glGenBuffers(NUM_GEOMETRY, &m_colorBuffer[0]);
 
 	//An Array of 3 vectors which represents 3 vertices
-	static const GLfloat vertex_buffer_data[] = {
-		-0.7f, 0.0f, 0.0f, //Vertice 0
-		0.0f, 1.0f, 0.0f, //Vertice 1
-		1.0f, 0.0f, 0.0f, //Vertice 2
-	};
+	//static const GLfloat vertex_buffer_data_1[] = {
+	//	-1.0f, 0.0f, 0.0f, // Vertex 0
+	//	0.0f, 1.0f, 0.0f, // Vertex 1
+	//	1.0f, 0.0f, 0.0f, // Vertex 2
+	//};
 
-	//second triangle
-	static const GLfloat vertex_buffer_data_2[] = {
-		0.0f, 0.7f, 0.0f, //Vertice 0
-		-1.0f, 0.0f, 0.0f, //Vertice 1
-		1.0f, 0.0f, 0.0f, //Vertice 2
+	//CHECK POINT 4, BLANKING OUT THE PREVIOUS COLOR/ BUFFER ARRAYS
+
+	static const GLfloat vertex_buffer_data_1[] = {
+		-0.5f, 0.7f, 0.0f,
+		0.5f, 0.7f, 0.0f,
+		-0.7f, 0.3f, 0.0f,
+		0.7f, 0.3f, 0.0f,
+		-0.7f, -0.2f, 0.0f,
+		0.7f, -0.2f, 0.0f,
+		-0.5f, -0.7f, 0.0f,
+		0.5f, -0.7f, 0.0f
 	};
 
 	//set the current active buffer
 	glBindBuffer(GL_ARRAY_BUFFER, m_vertexBuffer[GEO_TRIANGLE_1]);
-	//Transfer vertices to OpenGL
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_buffer_data), vertex_buffer_data, GL_STATIC_DRAW);
+	//Transfer Vertices to OpenGL
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_buffer_data_1), vertex_buffer_data_1, GL_STATIC_DRAW);
 
-	
+	//An array of 3 vectors which represents the colors of the 3 vertices
+	//static const GLfloat color_buffer_data_1[] = {
+	//	1.0f, 0.0f, 0.0f, // Color for Vertex 0
+	//	0.0f, 1.0f, 0.0f, // Color for Vertex 1
+	//	0.0f, 1.0f, 1.0f, // Color for Vertex 2
+	//};
+
+	static const GLfloat color_buffer_data_1[] = {
+		0.4f, 1.0f, 0.6f,
+		1.0f, 1.0f, 1.0f,
+		0.0f, 0.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+		1.0f, 0.0f, 1.0f,
+		1.0f, 0.1f, 0.0f,
+		1.0f, 1.0f, 0.0f,
+		1.0f, 0.0f, 1.0f,
+	};
+
+	glBindBuffer(GL_ARRAY_BUFFER, m_colorBuffer[GEO_TRIANGLE_1]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(color_buffer_data_1), color_buffer_data_1, GL_STATIC_DRAW);
+
+
+
+	//An Array of 3 vectors which represents 3 vertices
+	static const GLfloat vertex_buffer_data_2[] = {
+		0.5f, 0.7f, 0.0f, // Vertex 0
+		-0.7f, -0.2f, 0.0f, // Vertex 1
+		0.5f, -0.2f, 0.0f, // Vertex 2
+
+		0.5f, -0.7f, 0.0f, // Vertex 6
+		-0.1f, -0.2f, 0.0f, // Vertex 7
+		0.5f, -0.2f, 0.0f, // Vertex 8
+
+		-0.1f, -0.2f, 0.0f, // Vertex 3
+		-0.7f, 0.3f, 0.0f, // Vertex 4
+		0.7f, 0.3f, 0.0f, // Vertex 5
+	};
+
+	//set the current active buffer
 	glBindBuffer(GL_ARRAY_BUFFER, m_vertexBuffer[GEO_TRIANGLE_2]);
+	//Transfer Vertices to OpenGL
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_buffer_data_2), vertex_buffer_data_2, GL_STATIC_DRAW);
 
-	// An array of 3 vectors which represents the colors of the 3 vertices
-		static const GLfloat color_buffer_data[] = {
-		1.0f, 1.0f, 0.0f, //color of vertex 0
-		1.0f, 1.0f, 0.0f, //color of vertex 1
-		1.0f, 1.0f, 0.0f, //color of vertex 2
-		};
-
-		static const GLfloat color_buffer_data_2[] = {
-		1.0f, 0.0f, 0.0f, //color of vertex 0
-		1.0f, 0.0f, 0.0f, //color of vertex 1
-		1.0f, 0.0f, 0.0f, //color of vertex 2
-		};
-	glBindBuffer(GL_ARRAY_BUFFER, m_colorBuffer[GEO_TRIANGLE_1]);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(color_buffer_data),
-		color_buffer_data, GL_STATIC_DRAW);
-
+	//An array of 3 vectors which represents the colors of the 3 vertices
+	static const GLfloat color_buffer_data_2[] = {
+		-1.0f, 0.0f, 0.5f, // Color for Vertex 0
+		0.0f, 0.0f, 0.0f, // Color for Vertex 1
+		1.0f, 1.0f, 1.0f, // Color for Vertex 2
+		-1.0f, 0.0f, 0.5f, // Color for Vertex 3
+		0.0f, 0.0f, 0.0f, // Color for Vertex 4
+		1.0f, 1.0f, 1.0f, //Color for Vertex 5
+		-1.0f, 0.0f, 0.5f, // Color for Vertex 6
+		0.0f, 0.0f, 0.0f, // Color for Vertex 7
+		1.0f, 1.0f, 1.0f, //color for Vertex 8
+	};
 	glBindBuffer(GL_ARRAY_BUFFER, m_colorBuffer[GEO_TRIANGLE_2]);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(color_buffer_data_2),
-		color_buffer_data_2, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(color_buffer_data_2), color_buffer_data_2, GL_STATIC_DRAW);
 
+	static const GLfloat vertex_buffer_data_3[] = {
+		-0.1f, 0.0f, 0.0f,
+		0.2f, 0.1f, 0.0f,
+		-0.3f, 0.2f, 0.0f,
+		0.4f, 0.3f, 0.0f,
+		-0.5f, -0.4f, 0.0f,
+	};
 
+	//set the current active buffer
+	glBindBuffer(GL_ARRAY_BUFFER, m_vertexBuffer[GEO_TRIANGLE_3]);
+	//Transfer Vertices to OpenGL
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_buffer_data_3), vertex_buffer_data_3, GL_STATIC_DRAW);
 
+	//An array of 3 vectors which represents the colors of the 3 vertices
+	//static const GLfloat color_buffer_data_1[] = {
+	//	1.0f, 0.0f, 0.0f, // Color for Vertex 0
+	//	0.0f, 1.0f, 0.0f, // Color for Vertex 1
+	//	0.0f, 1.0f, 1.0f, // Color for Vertex 2
+	//};
 
+	static const GLfloat color_buffer_data_3[] = {
+		1.0f, 0.4f, 0.6f,
+		0.0f, 0.0f, 1.0f,
+		1.0f, 0.0f, 0.0f,
+		0.0f, 0.5f, 0.0f,
+		1.0f, 0.1f, 0.0f,
+	};
 
+	glBindBuffer(GL_ARRAY_BUFFER, m_colorBuffer[GEO_TRIANGLE_3]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(color_buffer_data_3), color_buffer_data_3, GL_STATIC_DRAW);
+
+	//DEPTH TESTING
+	//enable Depth Test
+	glEnable(GL_DEPTH_TEST);
 }
 
 void Scene1::Update(double dt)
@@ -83,54 +157,76 @@ void Scene1::Update(double dt)
 
 void Scene1::Render()
 {
-	//Clear colour buffer every frame (Updating per frame?)
-	glClear(GL_COLOR_BUFFER_BIT);
-	glEnableVertexAttribArray(0); //1st attribute buffer : vertices
-	glEnableVertexAttribArray(1); //2nd attribute buffer : colors
+	//clear color buffer every frame
+	//glClear(GL_COLOR_BUFFER_BIT); (Check point 4, currently getting replaced)
+	//New clear command! Clears color and depth buffer every frame
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glEnableVertexAttribArray(0); //1st attribute buffer : Vertices
 	glBindBuffer(GL_ARRAY_BUFFER, m_vertexBuffer[GEO_TRIANGLE_1]);
 	glVertexAttribPointer(
-		0, //attribute 0.Must match layout in the shader. Usually 0 is for vertex
+		0, // attribute 0. Must match the layout in the shader.Usually 0 is for vertex
 		3, //size
-		GL_FLOAT, //Type
-		GL_FALSE, //normalised?
+		GL_FLOAT, //type
+		GL_FALSE, //normalized?
 		0, //Stride
 		0 //array buffer offset
 	);
 
+	glEnableVertexAttribArray(1); //2nd attribute buffer : colors
 
 	glBindBuffer(GL_ARRAY_BUFFER, m_colorBuffer[GEO_TRIANGLE_1]);
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	//to draw triangle
-	glDrawArrays(GL_TRIANGLES, 0, 3); //Starting from vertex 0; 3 vertices would make 1 traingele
-	
+
+
+
+	//Draw the triangle
+	glDrawArrays(GL_TRIANGLE_STRIP, 0, 8); //starting from vertex 0; 3 vertices - 1 triangle
+
+	glEnableVertexAttribArray(0); //1st attribute buffer : Vertices
 	glBindBuffer(GL_ARRAY_BUFFER, m_vertexBuffer[GEO_TRIANGLE_2]);
 	glVertexAttribPointer(
-		0, //attribute 0.Must match layout in the shader. Usually 0 is for vertex
+		0, // attribute 0. Must match the layout in the shader.Usually 0 is for vertex
 		3, //size
-		GL_FLOAT, //Type
-		GL_FALSE, //normalised?
+		GL_FLOAT, //type
+		GL_FALSE, //normalized?
 		0, //Stride
 		0 //array buffer offset
 	);
 
-
+	glEnableVertexAttribArray(1); //2nd attribute buffer : colors
 	glBindBuffer(GL_ARRAY_BUFFER, m_colorBuffer[GEO_TRIANGLE_2]);
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	//to draw triangle
-	glDrawArrays(GL_TRIANGLES, 0, 3); //Starting from vertex 0; 3 vertices would make 1 traingele
+
+	glDrawArrays(GL_TRIANGLES, 0, 9); //starting from vertex 0; 3 vertices - 1 triangle
+
+	glEnableVertexAttribArray(0); //1st attribute buffer : Vertices
+	glBindBuffer(GL_ARRAY_BUFFER, m_vertexBuffer[GEO_TRIANGLE_3]);
+	glVertexAttribPointer(
+		0, // attribute 0. Must match the layout in the shader.Usually 0 is for vertex
+		3, //size
+		GL_FLOAT, //type
+		GL_FALSE, //normalized?
+		0, //Stride
+		0 //array buffer offset
+	);
+
+	glEnableVertexAttribArray(1); //2nd attribute buffer : colors
+	glBindBuffer(GL_ARRAY_BUFFER, m_colorBuffer[GEO_TRIANGLE_3]);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+	glDrawArrays(GL_TRIANGLES, 0, 5); //starting from vertex 0; 3 vertices - 1 triangle
 
 	glDisableVertexAttribArray(1);
-
 	glDisableVertexAttribArray(0);
+
 }
 
 void Scene1::Exit()
 {
-	// Cleanup VBO here
 	glDeleteBuffers(NUM_GEOMETRY, &m_vertexBuffer[0]);
 	glDeleteBuffers(NUM_GEOMETRY, &m_colorBuffer[0]);
 	glDeleteVertexArrays(1, &m_vertexArrayID);
 	glDeleteProgram(m_programID);
-		
+
 }
-//PLEASE
--- Application/Source/Scene1.h
+++ Application/Source/Scene1.h
@@ -6,25 +6,26 @@
 class Scene1 : public Scene
 {
 public:
-	Scene1();
-	~Scene1();
+    Scene1();
+    ~Scene1();
 
-	virtual void Init();
-	virtual void Update(double dt);
-	virtual void Render();
-	virtual void Exit();
+    virtual void Init();
+    virtual void Update(double dt);
+    virtual void Render();
+    virtual void Exit();
+    enum GEOMETRY_TYPE
+    {
+        GEO_TRIANGLE_1 = 0,
+        GEO_TRIANGLE_2,
+        GEO_TRIANGLE_3,
+        NUM_GEOMETRY,
+    };
 
-	enum GEOMETRY_TYPE
-	{
-		GEO_TRIANGLE_1 = 0,
-		GEO_TRIANGLE_2,
-		NUM_GEOMETRY,
-	};
 private:
-	unsigned m_vertexArrayID;
-	unsigned m_vertexBuffer[NUM_GEOMETRY];
-	unsigned m_colorBuffer[NUM_GEOMETRY];
-	unsigned m_programID;
+    unsigned m_vertexArrayID;
+    unsigned m_vertexBuffer[NUM_GEOMETRY];
+    unsigned m_colorBuffer[NUM_GEOMETRY];
+    unsigned m_programID;
 };
 
 #endif
